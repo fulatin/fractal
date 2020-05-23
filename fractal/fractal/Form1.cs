436db3d9f0edb0f2a5251b94b013cc9b77464383
XiaoShi-root <63101291+XiaoShi-root@users.noreply.github.com>
2020-05-23 16:56:40 +0800

    可以绘制谢尔并斯基三角

--- fractal/fractal/Form1.cs
+++ fractal/fractal/Form1.cs
@@ -21,21 +21,27 @@ namespace fractal
         {
             Graphics g;
             g = this.CreateGraphics();
-
+            long times = 0;
             Sierpinski_triangle sierpinski_Triangle = new Sierpinski_triangle(g);
-            sierpinski_Triangle.Draw();
+            while (times < +10000)
+            {
+                sierpinski_Triangle.Draw();
+            }
         }
     }
     public class Sierpinski_triangle
     {
         Graphics g;
         Pen Pen = new Pen(Color.Black, 0.1f);
-        public int[] a, b, c ,temppoint;
+        public int[] a, b, c;
+        public double[] temppoint;
+        public double a_Between_temppoint, b_Between_temppoint,c_Between_temppoint;
         public Sierpinski_triangle(Graphics graphics)
         {
             a = new int[] { 400,2 };
             b = new int[] { 2,500 };
             c = new int[] { 798, 500 };
+            temppoint = new double[] { 12.33f, 123.2f };
             g = graphics;
             g.DrawLine(Pen, a[0], a[1], a[0] + 0.1f, a[1] + 0.1f);
             g.DrawLine(Pen, b[0], b[1], b[0] + 0.1f, b[1] + 0.1f);
@@ -43,8 +49,34 @@ namespace fractal
         }
         public void Draw()
         {
-            
+            a_Between_temppoint = Get_Distance(a, temppoint);
+            b_Between_temppoint = Get_Distance(b, temppoint);
+            c_Between_temppoint = Get_Distance(c, temppoint);
+            if (a_Between_temppoint >= b_Between_temppoint)
+            {
+                if (a_Between_temppoint >= c_Between_temppoint)
+                {
+                    temppoint = Getmidpoint(a, temppoint);
+                }
+            }
+            else if(b_Between_temppoint >= c_Between_temppoint)
+            {
+                temppoint = Getmidpoint(b, temppoint);
+            }
+            else
+            {
+                temppoint = Getmidpoint(c, temppoint);
+            }
+            float x = (float)temppoint[0];
+            g.DrawLine(Pen, (float)temppoint[0], (float)temppoint[1], (float)(temppoint[0] + 0.1), (float)(temppoint[1] + 0.1));
         }
-
+        protected double Get_Distance(int[]a , double[] temp)
+        {
+            return Math.Sqrt(Math.Pow(a[0]-b[0],2)+Math.Pow(a[1]-b[1],2));
+        }
+        protected double[] Getmidpoint(int[] a, double[] temp)
+        {
+            return new double[] {(a[0]+temp[0])/2, (a[1] + temp[1]) / 2 };
+        }
     }
 } 

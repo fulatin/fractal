7b5dbf46f081641627d1aaf213b009d375e4f802
XiaoShi-root <63101291+XiaoShi-root@users.noreply.github.com>
2020-05-23 16:07:44 +0800

    初始化点

--- fractal/fractal/Form1.cs
+++ fractal/fractal/Form1.cs
@@ -19,7 +19,32 @@ namespace fractal
 
         private void Clickaction(object sender, EventArgs e)
         {
+            Graphics g;
+            g = this.CreateGraphics();
 
+            Sierpinski_triangle sierpinski_Triangle = new Sierpinski_triangle(g);
+            sierpinski_Triangle.Draw();
         }
     }
-}
+    public class Sierpinski_triangle
+    {
+        Graphics g;
+        Pen Pen = new Pen(Color.Black, 0.1f);
+        public int[] a, b, c ,temppoint;
+        public Sierpinski_triangle(Graphics graphics)
+        {
+            a = new int[] { 400,2 };
+            b = new int[] { 2,500 };
+            c = new int[] { 798, 500 };
+            g = graphics;
+            g.DrawLine(Pen, a[0], a[1], a[0] + 0.1f, a[1] + 0.1f);
+            g.DrawLine(Pen, b[0], b[1], b[0] + 0.1f, b[1] + 0.1f);
+            g.DrawLine(Pen, c[0], c[1], c[0] + 0.1f, c[1] + 0.1f);
+        }
+        public void Draw()
+        {
+            
+        }
+
+    }
+} 
